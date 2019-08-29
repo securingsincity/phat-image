54dfcb4939fed57c0cad8c6c89b4fad4158c8469
James Hrisho <james.hrisho@gmail.com>
2019-08-28 21:32:19 -0400

    Support spotify updating and some refactoring

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
-module github.com/securingsincity/what-image
+module github.com/securingsincity/phat-image
 
 go 1.12
 
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/zmb3/spotify v0.0.0-20190725171427-5159bf56b13d
 	golang.org/x/image v0.0.0-20190823064033-3a9bac650e44
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
 	periph.io/x/periph v0.0.0-20190814162326-ebc08ac5a62d
 )
--- main.go
+++ main.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/golang/freetype/truetype"
 	"github.com/securingsincity/phat-image/imagebuilder"
+	"github.com/zmb3/spotify"
 	"golang.org/x/image/font/gofont/gobold"
 	"golang.org/x/image/font/gofont/gobolditalic"
 )
 
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+
+var (
+	ch    = make(chan *spotify.Client)
+	state = "abc123ab3"
+)
+
 func main() {
 	const S = 400
-	var writeOutImage = false
+	redirectURI := GetString("SPOTIFY_REDIRECT_URL", "http://localhost:8080/callback")
+	var auth = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadCurrentlyPlaying)
+	var writeOutImage = true
 	var title = ""
 	var artist = ""
 	var style = 1
 	flag.StringVar(&title, "title", "", "name of the song")
 	flag.StringVar(&artist, "artist", "", "name of the song")
 	flag.IntVar(&style, "style", 1, "name of the song")
-	flag.BoolVar(&writeOutImage, "outto", false, "output images")
+	flag.BoolVar(&writeOutImage, "outto", true, "output images")
 	flag.Parse()
 
 	boldFont, err := truetype.Parse(gobold.TTF)
@@ -35,11 +53,75 @@ func main() {
 		Size: 16,
 	})
 
-	dc := imagebuilder.GenerateImage(style, boldFace, boldItalicFace, title, artist)
-	if writeOutImage == true {
-		imagebuilder.WriteToEink(dc)
-	} else {
-		dc.SavePNG("out.png")
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth(auth))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	t := time.Tick(time.Second * 20)
+
+	for range t {
+		currentPlaying, err := client.PlayerCurrentlyPlaying()
+		if err != nil {
+			log.Fatalf("FATAL line 46:%s", err)
+		}
+
+		newTitle := currentPlaying.Item.Name
+		newArtist := currentPlaying.Item.Artists[0].Name
+		if newTitle != title || newArtist != artist {
+			title = newTitle
+			artist = newArtist
+			log.Printf("Currently Playing: %v by %v", currentPlaying.Item.Name, currentPlaying.Item.Artists[0].Name)
+
+			rand.Seed(time.Now().UnixNano())
+			style = rand.Intn(4) + 1
+			dc := imagebuilder.GenerateImage(style, boldFace, boldItalicFace, title, artist)
+			if writeOutImage == true {
+				imagebuilder.WriteToEink(dc)
+			} else {
+				dc.SavePNG("out.png")
+			}
+
+		}
+
+	}
+}
+
+type Handler func(w http.ResponseWriter, r *http.Request)
+
+func completeAuth(auth spotify.Authenticator) Handler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+		tok, err := auth.Token(state, r)
+		if err != nil {
+			http.Error(w, "Couldn't get token", http.StatusForbidden)
+			log.Fatal(err)
+		}
+		if st := r.FormValue("state"); st != state {
+			http.NotFound(w, r)
+			log.Fatalf("State mismatch: %s != %s\n", st, state)
+		}
+		// use the token to get an authenticated client
+		client := auth.NewClient(tok)
+		fmt.Fprintf(w, "Login Completed!")
+		ch <- &client
+	}
+}
+
+func GetString(key string, defaultVal string) string {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return defaultVal
 	}
 
+	return val
 }
